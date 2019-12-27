b82382d8171235f720905fc18a537bdfadc6c1b8
JA <Jakea350@protonmail.com>
2019-12-27 12:57:42 -0600

    firefox prefs.js

---
+++ prefs.js
@@ -0,0 +1,24 @@
+// Firefox about:config file for decent privacy and security
+user_pref("beacon.enabled", false);
+user_pref("browser.privatebrowsing.autostart", true);
+user_pref("browser.safebrowsing.appRepURL", "");
+user_pref("browser.safebrowsing.malware.enabled", false);
+user_pref("browser.send_pings", false);
+user_pref("dom.battery.enabled", false);
+user_pref("extensions.CanvasBlocker@kkapsner.de.whiteList", "");
+user_pref("media.navigator.enabled", false);
+user_pref("media.peerconnection.enabled", false);
+user_pref("media.video_stats.enabled", false);
+user_pref("network.cookie.cookieBehavior", 1);
+user_pref("network.dns.disablePrefetch", true);
+user_pref("network.http.referer.spoofSource", true);
+user_pref("network.prefetch-next", false);
+user_pref("privacy.trackingprotection.enabled", true);
+user_pref("privacy.donottrackheader.enabled", true);
+user_pref("privacy.donottrackheader.value", 1);
+user_pref("privacy.clearOnShutdown.cache", true);
+user_pref("privacy.clearOnShutdown.cookies", true);
+user_pref("privacy.clearOnShutdown.downloads", true);
+user_pref("privacy.clearOnShutdown.sessions", true);
+user_pref("privacy.clearOnShutdown.history", true);
+user_pref("privacy.clearOnShutdown.formdata", true);
